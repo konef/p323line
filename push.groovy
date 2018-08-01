ed8e08ad43642790e4c1349e9f546a7a22a903d5
Evginiy <manukevich96@gmail.com>
2018-08-01 20:54:43 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,11 +1,12 @@
 def nexus_path = 'EPBYMINW7296/nexus'
-def group = 'org.jboss'
-def artifact = 'jboss-parent'
-def version = '23'
-def repo = 'project-releases'
+def group = "org.jboss"
+def artifact = "jboss-parent"
+def version = "23"
+def repo = "project-releases"
 def build_num = System.getenv("BUILD_NUMBER")
-def File = new File ("pipeline-ymaniukevich-${build_num}.tar.gz").getBytes()
-    def con = new URL("http://${nexus_path}/repository/${repo}/${group}/${artifact}/${version}/${artifact}-${version}.tar.gz").openConnection()
+
+    def File = new File ("pipeline-ymaniukevich-${build_num}.tar.gz").getBytes()
+    def con = new URL("http://${nexus_path}/repository/${repo}/${group}/${artifact}/${version}/pipeline-ymaniukevich-${build_num}.tar.gz").openConnection()
     def authString = "admin:admin123".getBytes().encodeBase64().toString()
     con.setRequestProperty( "Authorization", "Basic ${authString}")
     con.setRequestMethod("PUT")
