39765f944bee07f8df793e98fb79e6c7771d6543
EugenePapkou <eugene.papkou@gmail.com>
2018-08-02 17:18:32 +0300

    update push script

--- push.groovy
+++ push.groovy
@@ -4,10 +4,10 @@ import groovyx.net.http.*
 import java.util.regex.*
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
-def str = System.getenv("art_name")
+def str = System.getenv("student")
 def str2 = System.getenv("BUILD_NUMBER")
 
-def upload = new HTTPBuilder ("http://epbyminw1766/nexus/repository/project-releases/pipeline/1/1/pipeline-ypapkou-${str2}.tar.gz")
+def upload = new HTTPBuilder ("http://epbyminw1766/nexus/repository/project-releases/pipeline/1/1/pipeline-${str}-${str2}.tar.gz")
 upload.headers[ 'Authorization' ] = "Basic " + "nexus-service-user:nexus-service-user".getBytes('iso-8859-1').encodeBase64()
 upload.request(Method.PUT, ContentType.BINARY) {
     body = new File ("pipeline-ypapkou-${str2}.tar.gz").bytes
