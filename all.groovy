2e8399ed71bf59dbc4a82f93314b507beef73470
Darya Zhukova <Darya_Zhukova@epam.com>
2018-08-02 08:54:00 +0100

    push/pull script

---
+++ all.groovy
@@ -0,0 +1,62 @@
+@Grapes(
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+)
+import groovyx.net.http.RESTClient
+import org.apache.http.entity.*
+import hudson.model.*
+def encodeTarFile( Object data ) throws UnsupportedEncodingException {
+   def entity = new FileEntity( (File) data, "application/tar.gz" );
+   entity.setContentType( "application/tar.gz" );
+   return entity
+   }
+
+
+def upload() {
+   def WORKSPACE = System.getenv('WORKSPACE');
+   def BUILD_NUMBER = System.getenv('BUILD_NUMBER')
+   def repo = "maven-helloworld"
+   def gav = parse_gav()
+   def file = new File("pipeline-dzhukova-${BUILD_NUMBER}.tar.gz")
+   def rest = new RESTClient( "http://192.168.17.4/nexus/repository/${repo}/")
+        rest.auth.basic 'admin', 'admin'
+        rest.encoder.'application/zip' = this.&encodeZipFile
+        println "http://192.168.17.4/nexus/repository/${repo}/${gav[1]}/${BUILD_NUMBER}/${gav[1]}-${BUILD_NUMBER}.tar.gz"
+        rest.put(path: "${gav[1]}/${BUILD_NUMBER}/${gav[1]}-${BUILD_NUMBER}.tar.gz", body: file, requestContentType: 'application/zip')
+
+}
+def parse_gav() {
+def WORKSPACE = System.getenv('WORKSPACE');
+        def pom = new XmlSlurper().parse(new File("${WORKSPACE}/helloworld-ws/pom.xml"))
+        def gavs = []
+        gavs.add(pom.parent.groupId)
+        gavs.add(pom.artifactId)
+        gavs.add(pom.parent.version)
+        return gavs
+}
+def download() {
+   def gav = parse_gav()
+   def BUILD_NUMBER = System.getenv('BUILD_NUMBER')
+   def repo = "maven-helloworld"
+   def rest = new RESTClient( "http://192.168.17.4/nexus/repository/${repo}")
+        rest.auth.basic 'admin', 'admin'
+        def resp = rest.get( path:  "${gav[1]}/${BUILD_NUMBER}/${gav[1]}-${BUILD_NUMBER}.tar.gz")
+   new File("./app.tar.gz") << resp.data
+}
+
+def cli = new CliBuilder(usage:'ls')
+import org.apache.commons.cli.Option
+cli.with
+{
+   h(longOpt: 'help', 'Usage Information', required: false)
+   c(longOpt: 'option', 'push/pull', args: 1, required: true)
+}
+
+def options = cli.parse(args)
+println options.c
+if (options.c=='pull') {   
+        download()
+}
+if (options.c=='push') {
+        upload()
+}
+
