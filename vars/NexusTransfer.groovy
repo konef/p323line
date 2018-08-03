8b37fe7d7030696e533209c003d7a071d97a1985
Ratiborec <mikhailznak@gmail.com>
2018-08-03 12:37:34 +0100

    New file

---
+++ vars/NexusTransfer.groovy
@@ -0,0 +1,68 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+import groovyx.net.http.RESTClient
+import groovyx.net.http.*
+import org.apache.http.entity.*
+	
+void push() {
+  	def pom = new XmlSlurper().parse(System.getenv("WORKSPACE") +'/helloworld-ws/pom.xml')
+	def gr = pom.parent.groupId
+	def ar = pom.artifactId
+	def ver = pom.parent.version
+  	def restClient =  new RESTClient('http://epbyminw7425/nexus/repository/maven-archive/')
+	def workspace = System.getenv("WORKSPACE")
+  	def build = System.getenv("BUILD_NUMBER")
+	restClient.auth.basic 'jenkins', 'jenkins'
+	restClient.encoder.'application/zip' = this.&encodeZipFile
+  
+        def response = restClient.put(path: 'http://epbyminw7425/nexus/repository/maven-archive/' +
+				gr + '/' + 
+                                ar + '/' + 
+                                ver + '.' + build +'/' + 
+                                ar + '-'+ ver + '.' + build + '.tar.gz', 
+                                body: new File(workspace + '/pipeline-mznak-'+build+'.tar.gz'),
+                                requestContentType: 'application/zip')
+}
+
+
+def encodeZipFile(Object data) throws UnsupportedEncodingException {
+	def entity = new FileEntity((File) data, 'application/zip')
+	entity.setContentType('application/zip')
+	return entity
+}
+
+
+void pull() {  
+  	def pom = new XmlSlurper().parse(System.getenv("WORKSPACE") +'/helloworld-ws/pom.xml')
+	def gr = pom.parent.groupId
+	def ar = pom.artifactId
+	def ver = pom.parent.version
+  	def build = System.getenv("BUILD_NUMBER")
+	
+  	def restClient =  new RESTClient('http://epbyminw7425/nexus/repository/maven-archive/')
+	def workspace = System.getenv("WORKSPACE")
+  
+        def remoteUrl = 'http://epbyminw7425/nexus/repository/maven-archive/' + gr +
+        '/'+ ar +
+        '/'+ ver + '.' + build + 
+        '/' + ar + '-'+ ver + '.' + build + '.tar.gz'
+        def url = new URL(remoteUrl)
+        def authString = "jenkins:jenkins".getBytes().encodeBase64().toString()
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      
+        conn.setRequestProperty( "Authorization", "Basic ${authString}" )
+        new File(workspace + '/project.war').withOutputStream { out ->
+            out << conn.inputStream
+        }
+}
+
+
+def cli = new CliBuilder()
+def options = cli.parse(args)
+def arguments = options.arguments()
+
+if (arguments.size() != 0 && arguments[0] == 'pull' ){
+    pull()
+}
+else  if (arguments.size() != 0 && arguments[0] == 'push' ){
+    push()
+}
