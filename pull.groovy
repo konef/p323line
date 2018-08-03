f54f022b1af52691df7267400225a960532910ea
EugenePapkou <eugene.papkou@gmail.com>
2018-08-03 15:19:42 +0300

    update pull

--- pull.groovy
+++ pull.groovy
@@ -6,7 +6,7 @@ import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
 
 
-def http = new HTTPBuilder("http://epbyminw1766/nexus/service/siesta/rest/beta/search?repository=project-releases&group=customgroup&version=1")
+def http = new HTTPBuilder("http://epbyminw1766/nexus/service/siesta/rest/beta/search?repository=project-releases&group=pipeline2&version=1")
 http.headers['Authorization'] = "Basic " + "nexus-service-user:nexus-service-user".getBytes('iso-8859-1').encodeBase64()
 http.request(Method.GET) {
     response.success = { resp, json ->
