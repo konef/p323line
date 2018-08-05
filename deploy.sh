3655e8a25e83a49ece23d35e16c97d883dfa9952
Yauheni Sokal <31725642+SokolJam@users.noreply.github.com>
2018-08-05 20:50:32 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ home_dir=/home/vagrant/Jenkins
 tar -xvzf $home_dir/$1 -C $home_dir/
 rm $home_dir/$1
 
-if [[ -f $home_dir/helloworld-ws.war_old ]]
+if [[ -f $home_dir/backup/helloworld-ws.war_old ]]
    then 
-      sudo mv $home_dir/helloworld-ws.war_old $home_dir/helloworld-ws.war_old_v2
+      sudo mv $home_dir/backup/helloworld-ws.war_old $home_dir/backup/helloworld-ws.war_old_v2
 fi
 
 if [[ -f /opt/tomcat/webapps/helloworld-ws.war ]] 
    then
-      sudo mv /opt/tomcat/webapps/helloworld-ws.war $home_dir/helloworld-ws.war_old
+      sudo mv /opt/tomcat/webapps/helloworld-ws.war $home_dir/backup/helloworld-ws.war_old
 fi
 
 sudo mv $home_dir/helloworld-ws.war /opt/tomcat/webapps/
@@ -25,16 +25,16 @@ b=$?
 
 if [ "$a" -eq 0 ] && [ "$b" -eq 0 ] 
 then
-  [ -f $home_dir/helloworld-ws.war_old_v2 ] && sudo rm -rf $home_dir/helloworld-ws.war_old_v2
+  [ -f $home_dir/backup/helloworld-ws.war_old_v2 ] && sudo rm -rf $home_dir/backup/helloworld-ws.war_old_v2
   echo "OK. $a,$b"
   exit 0
 else
-  if [ -f $home_dir/helloworld-ws.war_old ]
+  if [ -f $home_dir/backup/helloworld-ws.war_old ]
   then
       sudo rm -rf /opt/tomcat/webapps/helloworld-ws.war
-      sudo mv $home_dir/helloworld-ws.war_old /opt/tomcat/webapps/helloworld-ws.war
+      sudo mv $home_dir/backup/helloworld-ws.war_old /opt/tomcat/webapps/helloworld-ws.war
   fi
-  [ -f $home_dir/helloworld-ws.war_old_v2 ] && sudo mv $home_dir/helloworld-ws.war_old_v2 $home_dir/helloworld-ws.war_old
+  [ -f $home_dir/backup/helloworld-ws.war_old_v2 ] && sudo mv $home_dir/backup/helloworld-ws.war_old_v2 $home_dir/backup/helloworld-ws.war_old
   echo "FAIL - return to previous version. $a,$b"
   exit 1
 fi
